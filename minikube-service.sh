a5bc63cb0f7cf711973af361b166b1d211fd0703
Chris Snow <chsnow123@gmail.com>
2020-11-10 15:23:15 +0000

    remove minikub service

--- minikube-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NIFI_SERVICE=$(kubectl get service -l kubedirector.hpe.com/kdcluster=nifi | grep ^s | cut -d" " -f1)
-
-NIFI_PORT=$(minikube service $NIFI_SERVICE | tail -n 1 | cut -d: -f3)
-
-echo "If using kubedirector-lab, run the following on your host to forward ports:"
-echo
-echo "vagrant ssh -- -L $NIFI_PORT:localhost:$NIFI_PORT"
